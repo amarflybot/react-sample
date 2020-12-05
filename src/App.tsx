8346eceeab61972ba65d46288cdc7c482a229411
amarendrakumar <amarendra.kumar@thoughtworks.com>
2020-12-05 23:31:02 +0530

    Change for Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+function Button(props: { onClickFunction: (arg0: any) => any; increment: React.ReactNode; }) {
+  const handleClick = () => props.onClickFunction(props.increment);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={handleClick}>
+        +{props.increment}
+      </button>
+  );
+}
+
+function Display(props: { message: React.ReactNode; }) {
+  return (
+      <div>{props.message}</div>
+  );
+}
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (incr: number) => setCounter(counter+incr);
+  return (
+      <div>
+        <Button onClickFunction={incrementCounter} increment={1} />
+        <Button onClickFunction={incrementCounter} increment={5} />
+        <Button onClickFunction={incrementCounter} increment={10} />
+        <Button onClickFunction={incrementCounter} increment={20} />
+        <Display message={counter}/>
+      </div>
   );
 }
 
