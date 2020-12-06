a926897ea9c4e25f3cffef5fd2a0476e6c6896a4
amarendrakumar <amarendra.kumar@thoughtworks.com>
2020-12-06 20:27:32 +0530

    Taking input from user

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import Axios from "axios";
 
+/*
 const testData = [
     {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
     {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
     {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"}]
+*/
+
+const axiosInstance = Axios.create();
 
 const CardList = (props: { profileData: CardProps[]; }) => (
     <div>
-        {props.profileData.map(value => <Card {...value}/>)}
+        {props.profileData.map(value => <Card key={value.id} {...value}/>)}
     </div>
 );
 
@@ -17,17 +22,26 @@ class CardProps {
     name: string = '';
     avatar_url: string = '';
     company: string = ''
+    id: string = '';
 }
 
-export class Form extends React.Component {
+class FormProps {
+    onSubmit = (data: any): void => {
+    };
+}
 
-    private userNameInput: React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
+export class Form extends React.Component<FormProps> {
 
     state = {username: ''}
-
-    handleSubmit = (event: { preventDefault: () => void; }) => {
+    handleSubmit = async (event: { preventDefault: () => void; }) => {
         event.preventDefault();
-        console.log(this.state.username);
+        try {
+            let response = await axiosInstance.get(`https://api.github.com/users/${this.state.username}`);
+            this.props.onSubmit(response.data);
+        } catch (e) {
+            console.error(e);
+        }
+        this.setState({username: ''})
     };
     render() {
         return (
@@ -48,7 +62,7 @@ export class Card extends React.Component<CardProps> {
     render() {
         return (
             <div className="github-profile" style={{margin: '1rem'}}>
-                <img src={this.props.avatar_url}/>
+                <img src={this.props.avatar_url} alt='profile' style={{height:150, width:150}}/>
                 <div className="info" style={{display: 'inline-block', marginLeft: 10}}>
                     <div className="name" style={{fontSize: '125%'}}>{this.props.name}</div>
                     <div className="company">{this.props.company}</div>
@@ -60,14 +74,17 @@ export class Card extends React.Component<CardProps> {
 
 export class App extends React.Component {
 
-    /*constructor(props: { profiles: CardProps[]}) {
-        super(props);
-        this.state = {
-            profiles: testData,
-        };
-    }*/
     state = {
-        profiles: testData,
+        profiles: [],
+    }
+
+    addNewProfile = (profileData: any) => {
+        this.setState(prevState => ({
+            //console.log('App', profileData)
+            // @ts-ignore
+            profiles: [...prevState.profiles, profileData],
+        }));
+        //this.state.profiles.push({name: profileData.name, avatar_url: profileData.avatar_url, company: profileData.company})
     }
 
     render() {
@@ -76,7 +93,7 @@ export class App extends React.Component {
             <div className="header">
             The Github Cards App
             </div>
-            <Form/>
+            <Form onSubmit={this.addNewProfile}/>
             <CardList profileData={this.state.profiles}/>
             </>
         );
