755d4c19cd187886e977f0ce97b37f63e309b931
Amarendra Kumar <amarendra.kumar@thoughtworks.com>
2022-01-27 19:44:58 +0530

    Update packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,179 +1,73 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import utils from "./utils";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import {BrowserRouter as Router} from "react-router-dom";
+import {
+    MDBCollapse, MDBContainer,
+    MDBIcon,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarNav,
+    MDBNavbarToggler,
+    MDBNavItem,
+    MDBNavLink
+} from "mdbreact";
+export function App() {
 
-// Color Theme
-const colors: Map<string, string> = new Map([
-    ['available', 'lightgray'],
-    ['used', 'lightgreen'],
-    ['wrong', 'lightcoral'],
-    ['candidate', 'deepskyblue'],
-]);
+    const [collapse, setCollapse] = useState(false);
 
-
-type PlayNumberProps = {
-    number: number,
-    status: string,
-    onclick: (number: number, status: string) => void,
-}
-
-export class PlayNumber extends React.Component<PlayNumberProps> {
-    render() {
-        return (
-            <button
-                key={this.props.number}
-                className="number"
-                style={{backgroundColor: colors.get(this.props.status)}}
-                onClick={() => this.props.onclick(this.props.number, this.props.status)}
-            >
-                {this.props.number}
-            </button>);
+    function onClick() {
+        setCollapse(!collapse);
     }
-}
-
-export class StarsDisplay extends React.Component<{ stars: number }> {
-    render() {
-        return (
-            <>
-                {utils.range(1, this.props.stars).map(starId =>
-                    <div key={starId} className='star'/>
-                )} </>
-        );
-    }
-}
-
-export class PlayAgain extends React.Component<{ onclick: () => void , gameStatus: string}> {
-    render() {
-        return (
-            <div className="game-done">
-                <div
-                    className="message"
-                    style={{color: this.props.gameStatus === 'lost' ? 'red' : 'green'}}
-                >
-                    {this.props.gameStatus === 'lost' ? 'Game Over' : 'Nice'}
-                </div>
-                <button
-                    onClick={this.props.onclick}
-                >
-                    Play Again
-                </button>
-            </div>
-        );
-    }
-}
-
-function App() {
-    const [gameId, setGameId] = useState(1);
-
-    return <Game
-        key={gameId}
-        startNewGame={() => setGameId(gameId+1)} />;
-}
-
-function useGameState() {
-    const [stars, setStars] = useState(utils.random(1, 9));
-    const [availableNumbers, setAvailableNumbers] = useState(utils.range(1, 9));
-    const [candidateNumbers, setCandidateNumbers] = useState([0]);
-    const [secondsLeft, setSecondsLeft] = useState(10);
-
-
-    useEffect(() => {
-        if (secondsLeft > 0 && availableNumbers.length > 0) {
-            const timerId = setTimeout(() => {
-                setSecondsLeft(secondsLeft - 1)
-            }, 1000);
-            return () => {
-                clearTimeout(timerId);
-            }
-        }
-    });
-
-    const setGameState = (newCandidateNums: number[]) => {
-        if (utils.sum(newCandidateNums) !== stars) {
-            setCandidateNumbers(newCandidateNums);
-        } else {
-            let newAvailableNumbers = availableNumbers.filter(value => !newCandidateNums.includes(value));
-            setAvailableNumbers(newAvailableNumbers);
-            setCandidateNumbers([]);
-            setStars(utils.randomSumIn(newAvailableNumbers, 9));
-        }
-    };
-
-    return {
-        stars,
-        availableNumbers,
-        candidateNumbers,
-        secondsLeft,
-        setGameState
-    };
-}
-
-const Game: React.FC<{startNewGame: () => void}> = (props) => {
-
-    const {
-        stars,
-        availableNumbers,
-        candidateNumbers,
-        secondsLeft,
-        setGameState
-    } = useGameState();
-
-    const gameStatus = availableNumbers.length === 0
-        ? 'won'
-        : secondsLeft === 0 ? 'lost' : 'active';
-
-    const candidateAreWrong = () => {
-        return utils.sum(candidateNumbers) > stars;
-    };
-
-    const numberStatus = (number: number) => {
-        if (!availableNumbers.includes(number)) {
-            return 'used';
-        }
-        // @ts-ignore
-        if (candidateNumbers.includes(number)) {
-            return candidateAreWrong() ? 'wrong' : 'candidate';
-        }
-        return 'available';
-    };
-
-    const onNumberClick = (number: number, currentStatus: string) => {
-        if (currentStatus === 'used' || secondsLeft === 0) {
-            return;
-        }
-        const newCandidateNums = currentStatus === 'available'
-            ? candidateNumbers.concat(number)
-            : candidateNumbers.filter(value => value !== number);
-        setGameState(newCandidateNums);
-    }
-
+    const container = { height: 1300 }
     return (
-        <div className="game">
-            <div className="help">
-                Pick 1 or more numbers that sum to the number of stars
-            </div>
-            <div className="body">
-                <div className="left">
-                    {gameStatus !== 'active' ? (
-                        <PlayAgain onclick={props.startNewGame} gameStatus={gameStatus}/>
-                    ) : (
-                        <StarsDisplay stars={stars}/>
-                    )}
-                </div>
-                <div className="right">
-                    {utils.range(1, 9).map(number =>
-                        <PlayNumber
-                            key={number}
-                            status={numberStatus(number)}
-                            number={number}
-                            onclick={onNumberClick}
-                        />
-                    )}
-                </div>
-            </div>
-            <div className="timer">Time Remaining: {secondsLeft}</div>
+        <div>
+            <Router>
+                <header>
+                    <MDBNavbar color="default-color" dark expand="md">
+                        <MDBNavbarBrand href="/">
+                            <strong>Navbar</strong>
+                        </MDBNavbarBrand>
+                        <MDBNavbarToggler onClick={onClick} />
+                        <MDBCollapse isOpen={collapse} navbar>
+                            <MDBNavbarNav left>
+                                <MDBNavItem active>
+                                    <MDBNavLink to="#">Home</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#">Features</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#">Pricing</MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#">Opinions</MDBNavLink>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                            <MDBNavbarNav right>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#"><MDBIcon fab icon="facebook-f" /></MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#"><MDBIcon fab icon="twitter" /></MDBNavLink>
+                                </MDBNavItem>
+                                <MDBNavItem>
+                                    <MDBNavLink to="#"><MDBIcon fab icon="instagram" /></MDBNavLink>
+                                </MDBNavItem>
+                            </MDBNavbarNav>
+                        </MDBCollapse>
+                    </MDBNavbar>
+                </header>
+            </Router>
+            <MDBContainer style={container} className="text-center mt-5">
+                <h2>This Navbar isn't fixed</h2>
+                <h5>When you scroll down it will disappear</h5>
+            </MDBContainer>
         </div>
     );
+
 }
 
 export default App
